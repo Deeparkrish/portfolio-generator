03ffd0a7e42b11cbe9ce14679e8e038eafe8ecaf
Deepa <kosalai17@gmail.com>
2021-06-25 22:24:08 -0700

    webpage

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 
 const fs = require('fs');
-
 const generatePage = require('./src/page-template.js');
-
-
-
 const inquirer = require('inquirer');
+
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -105,10 +103,11 @@ Add a New Project
     portfolioData.projects.push(projectData);
     if (projectData.confirmAddProject) {
       return promptProject(portfolioData);
-    } else 
+    } else {
       return portfolioData;
+    }
     
-  })
+  });
  
 };
 
@@ -116,7 +115,7 @@ promptUser()
   .then(promptProject)
   .then(portfolioData => {
     const pageHTML=generatePage(portfolioData);
-    fs.writeFile('./index.html', generatePage(name, github), err => {
+    fs.writeFile('./index.html', pageHTML, err => {
     if (err) throw new Error(err);
     console.log('Portfolio complete! Check out index.html to see the output!');
   });
